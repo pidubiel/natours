cd4c7b8240950cf7ea2224fe9fcf2aea29924cd7
Piotr Dubiel <piotrdubiel@icloud.com>
2019-07-24 18:45:55 +0200

    Add tours pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -42,6 +42,18 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    // 4) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    // page=2&limit=10 1-10 page 1, 11-20 page 2, 21-30 page 3
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist!');
+    }
+
     // Execute query
     const tours = await query;
 
