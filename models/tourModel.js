1d84823dc437d11831495a0e7d1e0acc0b2e5f29
Piotr Dubiel <piotrdubiel@icloud.com>
2019-08-03 12:17:00 +0200

    Create built-in validators

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,7 +6,9 @@ const tourSchema = new mongoose.Schema(
     name: {
       type: String,
       required: [true, 'Tour must have a name'],
-      unique: true
+      unique: true,
+      maxlength: [40, 'A tour name must have less or equal than 40 characters'],
+      minlength: [10, 'A tour name must have more or equal than 10 characters']
     },
     slug: {
       type: String
@@ -21,11 +23,17 @@ const tourSchema = new mongoose.Schema(
     },
     difficulty: {
       type: String,
-      required: [true, 'A tour must have a difficulty']
+      required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium, difficult'
+      }
     },
     ratingsAverage: {
       type: Number,
-      default: 4.5
+      default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0']
     },
     ratingsQuantity: {
       type: Number,
@@ -87,6 +95,16 @@ tourSchema.pre(/^find/, function(next) {
 tourSchema.post(/^find/, function(docs, next) {
   console.log(`Query took ${Date.now() - this.start} milliseconds!`);
   //console.log(docs);
+  next();
+});
+
+//AGGREGATION MIDDLEWARE
+
+tourSchema.pre('aggregate', function(next) {
+  this.pipeline().unshift({
+    $match: { secretTour: { $ne: true } }
+  });
+  console.log(this.pipeline());
   next();
 });
 
