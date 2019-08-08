7f403cfc8abc3438888541ee32a2bd4011cc2c51
Piotr Dubiel <piotrdubiel@icloud.com>
2019-08-08 13:07:53 +0200

    Implement global error handling

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 //const fs = require('fs');
 const express = require('express');
 const morgan = require('morgan');
+
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -31,11 +34,11 @@ app.use('/api/v1/users', userRouter);
 
 //all means all http verbs (GET, POST ...)
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl}`
-  });
+  next(new AppError(`Can't find ${req.originalUrl}`, 404));
 });
+
+//Error handling middleware
+app.use(globalErrorHandler);
 
 // 4) START SERVER
 module.exports = app;
