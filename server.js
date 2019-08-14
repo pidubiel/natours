297bb03acd438e43573658eb9661d7ee527c0b46
Piotr Dubiel <piotrdubiel@icloud.com>
2019-08-14 16:20:27 +0200

    Process unhandled rejections and uncaught exceptions

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtExcepion', err => {
+  console.log(err.name, err.message);
+  console.log('Uncaught Excepion! Shutting down...');
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
@@ -22,6 +28,14 @@ mongoose
   .catch(() => console.log('DB connection failed'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message);
+  console.log('Unhandled rejection! Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
