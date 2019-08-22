5767d7db9d349026c798f550473d34de01e8ef22
Piotr Dubiel <piotrdubiel@icloud.com>
2019-08-22 21:31:45 +0200

    Handle JWT errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -26,6 +26,10 @@ const sendErrorDev = (err, res) => {
   });
 };
 
+const handleJWTError = () => new AppError('Invalid token. Please log in again', 401);
+
+const handleJWTExpiredError = () => new AppError('Your token has expired. Please log in again', 401);
+
 const sendErrorProd = (err, res) => {
   // Operational, trusted error: send message to client
   if (err.isOperational) {
@@ -52,8 +56,9 @@ module.exports = (err, req, res, next) => {
     let error = { ...err };
     if (error.name === 'CastError') error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
-    if (error.name === 'ValidationError')
-      error = handleValidationErrorDB(error);
+    if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
+    if (error.name === 'JsonWebTokenError') error = handleJWTError();
+    if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
     sendErrorProd(error, res);
   }
 };
